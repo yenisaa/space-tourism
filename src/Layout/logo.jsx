1536625cd0fa3acfbc60b819ae46d57ce4ffe2c3
Isaiah Yenou <ayomidesconcept@gmail.com>
2025-09-04 21:59:09 +0100

    remove node_modules from repo

--- src/Layout/logo.jsx
+++ src/Layout/logo.jsx
@@ -1,20 +1,83 @@
-import icon from '../assets/shared/logo.svg'
-import burgerMenu from '../assets/shared/icon-hamburger.svg'
+import { useState } from "react";
+import icon from "../assets/shared/logo.svg";
+import burgerMenu from "../assets/shared/icon-hamburger.svg";
+import closeIcon from "../assets/shared/icon-close.svg";
+import navLine from "../assets/shared/line.svg";
+import { NavLink } from "react-router-dom";
 
 function Logo() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="w-full max-w-[1110px] mx-auto flex justify-between items-center px-4 py-4">
+    <header className="flex justify-between items-center px-[64px] md:p-0 lg:px-[64px] w-full lg:w-full relative z-50">
       {/* Logo */}
-      <div>
-        <img src={icon} alt="Space tourism logo" className="w-10 h-10" />
+      <div className=" flex gap-[64px] justify-start md:px-[64px] w-full">
+        <img src={icon} alt="logo" className="w-[48px] h-[48px]" />
+        <img src={navLine} alt="navigation line" className="hidden lg:flex w-[560px] z-90" />
       </div>
 
-      {/* Burger menu (mobile only) */}
-      <button className="md:hidden">
-        <img src={burgerMenu} alt="Open navigation menu" className="w-6 h-6" />
-      </button>
+      {/* Burger button */}
+      <div className="flex w-full justify-end">
+        <button
+          className="md:hidden justify-end"
+          onClick={() => setIsOpen(true)}
+          aria-label="Open menu"
+        >
+          <img src={burgerMenu} alt="menu" className="w-6 h-6" />
+        </button>
+      </div>
+
+      {/* Desktop nav */}
+      <div className="flex-2">
+        <nav className="hidden md:flex gap-8 text-white uppercase inset-0 bg-black/50 bg-white/2 backdrop-blur-lg w-full lg:w-[736px] md:w-[640px] md:h-[96px] lg:h-[96px] md:items-center lg:items-center md:px-[40px] md:justify-end lg:justify-end py-4 font-lead  tracking-wider ">
+          <NavLink to="/">
+            <span className="font-bold">00</span> Home
+          </NavLink>
+          <NavLink to="/destination">
+            <span className="font-bold">01</span> Destination
+          </NavLink>
+          <NavLink to="/crew">
+            <span className="font-bold">02</span> Crew
+          </NavLink>
+          <NavLink to="/technology">
+            <span className="font-bold">04</span> Technology
+          </NavLink>
+        </nav>
+      </div>
+
+      {/* Mobile menu */}
+      {isOpen && (
+        <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-end justify-end w-full">
+          <div className="w-full max-w-[250px] h-full bg-white/5 backdrop-blur-lg text-white p-6 flex flex-col gap-6 self-end">
+            {/* Close button */}
+            <button
+              className="items-end"
+              onClick={() => setIsOpen(false)}
+              aria-label="Close menu"
+            >
+              <img src={closeIcon} alt="close menu" className="w-6 h-6" />
+            </button>
+
+            {/* Links */}
+            <nav className="flex flex-col gap-4 uppercase font-lead tracking-wider">
+              <NavLink to="/" onClick={() => setIsOpen(false)}>
+                <span className="font-bold">00</span> Home
+              </NavLink>
+              <NavLink to="/destination" onClick={() => setIsOpen(false)}>
+                <span className="font-bold">01</span> Destination
+              </NavLink>
+              <NavLink to="/crew" onClick={() => setIsOpen(false)}>
+                <span className="font-bold">02</span> Crew
+              </NavLink>
+              <NavLink to="/technology" onClick={() => setIsOpen(false)}>
+                <span className="font-bold">03</span> Technology
+              </NavLink>
+            </nav>
+          </div>
+        </div>
+      )}
     </header>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,27 +8,27 @@ function Home() {
         bg-[url('/background-home-mobile.jpg')]
         md:bg-[url('/background-home-tablet.jpg')]
         lg:bg-[url('/background-home-desktop.jpg')]
-        bg-center bg-cover h-screen 
+        bg-center bg-cover h-screen w-screen
         @container flex flex-col items-center  md:items-center justify-center p-5 relative"
     >
-        <div className="absolute top-5 left-5">
+        <div className="absolute top-5 w-full max-w-[1440px]">
             <Logo />
         </div>
 
-      <div className="md:w-[1110px] flex items-center justify-center gap-5">
+      <div className="lg:max-w-[1110px] flex items-center justify-center gap-5 ">
           <div
-            className="flex flex-col md:flex-row items-center
-          justify-between gap-2 w-full"
+            className="flex flex-col lg:flex-row items-center
+          justify-between gap-5  w-full "
           >
-            <div className="flex flex-col items-center md:items-start justify-center w-[540px] text-white text-center md:text-left mx-[24px] self-center">
+            <div className="flex flex-col items-center lg:items-start justify-center text-white text-center md:text-left mx-[24px] self-center">
               <div className="">
-                <p className="font-lead tracking-[.2rem] md:text-preset-4 text-(--font-blue-300)">SO, YOU WANT TO TRAVEL TO </p>
+                <p className="text-center font-lead tracking-[.2rem] md:text-preset-4 text-(--font-blue-300)">SO, YOU WANT TO TRAVEL TO </p>
               </div>
               <div className="">
                 <h1 className="font-heading text-[80px] md:text-[144px]">SPACE</h1>
               </div>
               <div className="w-[327px] md:w-[540px]">
-                <p className="font-paragraph tracking-wide leading-8">
+                <p className="font-paragraph tracking-wide leading-8 md:text-center lg:text-left">
                   Let’s face it; if you want to go to space, you might as well
                   genuinely go to outer space and not hover kind of on the edge of
                   it. Well sit back, and relax because we’ll give you a truly out of
